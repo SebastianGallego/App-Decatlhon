44358487c80e308485b34621ac9f63c15bae267c
Sebastian Gallego <sebastian.gallego@gmail.com>
2023-10-23 09:46:02 -0300

    product

--- product.js
+++ product.js
@@ -10,8 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const fetchData = async () => {
   const res = await fetch("api.json");
   data = await res.json();
-  console.log(data, id);
-  productFilter(data, "id", id);
+  productFilter(data, id);
 };
 
 let resultsContainer = document.getElementById("results");
@@ -19,43 +18,45 @@ let resultsContainer = document.getElementById("results");
 //Filtra por Campo/Valor, recibe el Array de Productos, y por que campo va
 // a filtrar ademas del valor, si recibe "Ver todos" muestra todos
 // Ademas actualiza el Titulo de la lista de Productos
-function productFilter(data, campo, valor) {
+function productFilter(data, valor) {
   const productsFilters = data.filter((producto) => {
-    return producto[campo] == valor || valor === "Ver Todos";
+    return producto.id == valor;
   });
-
-  //const resultsTitle = document.getElementById("resultsTitle");
-  //resultsTitle.innerHTML = "";
-
-  console.log(productsFilters);
-
-  //renderResults(productsFilters);
+  renderResults(productsFilters);
 }
 
-/*
-//Recibe el json de la consulta y genera las cards guardo el ID
-let productContainer = document.querySelector(".product-item");
+let productContainer = document.getElementById("result");
 
-function getResult(product) {
+//Recibe el json de la consulta y genera las cards guardo el ID
+function renderResults(product) {
   productContainer.innerHTML = `
-        <div class="shop-detail" id="${product.id}">  
-              <span class="product-title">${product.title}</span>
-              <img class="shop-item-image" src="${product.image}">
-              <span class="shop-item-description">${product.description}</span>
-              <div class="shop-item-details">
-                <span class="shop-item-price">$ ${product.price}</span>
-                <button class="btn btn-primary addCart-btn" type="button">Agregar al Carrito</button>
-                <button class="btn btn-primary cancel-btn" type="button" >Cancelar</button>
-              </div>
-          </div>`;
+        <div class="productSelected" id="${product[0].id}">  
+       
+
+        <div class="imgContainer" >  
+          <img class="shop-item-image" src="${product[0].thumbnailUrl}">
+        </div>
+        <div class="productContainer" >  
+          <h2 class="shop-item-description">${product[0].brand}</h2>
+          <H1 class="product-title">${product[0].title}</H1>
+          <h3 class="shop-item-price">$ ${product[0].price}</h3>
+          <p class="shop-item-description">${product[0].rating}</p>
+          <p class="shop-item-description">${product[0].details}</p>
+          <h4 class="shop-item-description">Color: ${product[0].color}</h4> 
+         </div>
+        
+         <button class="btn btn-primary addCart-btn" type="button">Agregar al Carrito</button>
+         <button class="btn btn-primary cancel-btn" type="button" >Cancelar</button>
+              
+       </div>`;
 
   //Escuchar los botones para ver que hacer
   // Agregar al carrito o Volver al index
 }
 
-const logoLink = document.getElementById("logo");
-const titleLink = document.getElementById("title");
-
+//const logoLink = document.getElementById("logo");
+//const titleLink = document.getElementById("title");
+/*
 logoLink.addEventListener("click", function () {
   pageback();
 });
