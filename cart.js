eff1cd31836d277afa1f5e44f6d8270cb0c0b17c
Sebastian Gallego <sebastian.gallego@gmail.com>
2023-10-26 15:55:00 -0300

    falta + y -. y el responsive de procuto

--- cart.js
+++ cart.js
@@ -11,6 +11,9 @@ document.addEventListener("DOMContentLoaded", () => {
   if (localStorage.getItem("cartStorage")) {
     cart = loadCart(); // Cargo los productos del LocalStorage al carrito si existen
     productRows.innerHTML = "";
+
+    console.log(cart);
+
     renderCart();
   }
 });
@@ -28,9 +31,9 @@ function renderCart() {
             </td>
         <td>${product.price} €</td>
       <td>
-          <button class="btn btn-danger">-</button>
+          <button class="btn btn-danger" dataset.id = ${product.id}>-</button>
           <span class="quantity">${product.quantity}</span>
-          <button class="btn btn-success">+</button>
+          <button class="btn btn-success" dataset.id = ${product.id}>+</button>
       </td>
           <td>${product.quantity * product.price} €</td>
       <td>
@@ -107,3 +110,83 @@ btnProductsPay.addEventListener("click", () => {
     Swal.fire("Carrito Vacío!", "No hay productos para pagar.", "warning");
   }
 });
+
+productRows.addEventListener("click", (e) => {
+  btnCantidad(e);
+});
+
+const btnCantidad = (e) => {
+  console.log(e);
+  if (e.target.classList.contains("btn-success")) {
+    console.log(e.target.dataset.id);
+    //producto.cantidad++;
+    //cart[e.target.dataset.id] = { ...producto };
+    //renderCart();
+  }
+
+  /*
+  if (e.target.classList.contains("btn-danger")) {
+    const producto = cart[e.target.dataset.id];
+    producto.cantidad--;
+    if (producto.cantidad === 0) {
+      delete cart[e.target.dataset.id];
+    } else {
+      cart[e.target.dataset.id] = { ...producto };
+    }
+    renderCart();
+  }*/
+  e.stopPropagation();
+};
+
+/* Plantilla Modal de contacto
+
+<div class="modal fade" id="modalContactForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header text-center">
+        <h4 class="modal-title w-100 font-weight-bold">Write to us</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body mx-3">
+        <div class="md-form mb-5">
+          <i class="fas fa-user prefix grey-text"></i>
+          <input type="text" id="form34" class="form-control validate">
+          <label data-error="wrong" data-success="right" for="form34">Your name</label>
+        </div>
+
+        <div class="md-form mb-5">
+          <i class="fas fa-envelope prefix grey-text"></i>
+          <input type="email" id="form29" class="form-control validate">
+          <label data-error="wrong" data-success="right" for="form29">Your email</label>
+        </div>
+
+        <div class="md-form mb-5">
+          <i class="fas fa-tag prefix grey-text"></i>
+          <input type="text" id="form32" class="form-control validate">
+          <label data-error="wrong" data-success="right" for="form32">Subject</label>
+        </div>
+
+        <div class="md-form">
+          <i class="fas fa-pencil prefix grey-text"></i>
+          <textarea type="text" id="form8" class="md-textarea form-control" rows="4"></textarea>
+          <label data-error="wrong" data-success="right" for="form8">Your message</label>
+        </div>
+
+      </div>
+      <div class="modal-footer d-flex justify-content-center">
+        <button class="btn btn-unique">Send <i class="fas fa-paper-plane-o ml-1"></i></button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="text-center">
+  <a href="" class="btn btn-default btn-rounded mb-4" data-toggle="modal" data-target="#modalContactForm">Launch
+    Modal Contact Form</a>
+</div>
+
+
+*/
